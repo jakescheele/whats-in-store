342b57b944d6af7238450e0248c79530f741d3b3
Jonathan Hernandez <jon.hernandez169@gmail.com>
2019-03-29 15:00:09 -0500

    jons first push

--- models/category.js
+++ models/category.js
@@ -1,8 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const categorySchema = new Schema({
-    
+  name: { type: String },
+  subcategories: [Schema.Types.ObjectId],
 });
 
 const Category = mongoose.model("Category", categorySchema);
--- server.js
+++ server.js
@@ -2,21 +2,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require("./routes")
+
 const app = express();
 
-// Set up dependencies
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 // Serve static assets
 if (process.env.NODE_ENV === "production") {
  app.use(express.static("client/build"));
 };
+// Set up dependencies
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(session({
+  secret:"secretSauce",
+  saveUninitialized:false,
+  resave:false
+}))
+app.use(passport.initialize())
+app.use(passport.session());
 
 // Add routes
 app.use(routes);
+const passportRote = require("./routes/auth")(passport);
+require("./passport")(passport);
+app.use('/auth', passportRote);
 
 // Connect to mongo db
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/reactreadinglist");
 
 // Listener
 app.listen(PORT, function () {
