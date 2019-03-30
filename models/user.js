f09ef4caa31d4ac418997b04cb292123069d9f07
ercasale97 <ercasale97@gmail.com>
2019-03-30 13:35:44 -0500

    set up start of components

--- models/user.js
+++ models/user.js
@@ -1,11 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    email:      {type: String, required: true,},
-    password:   {type: String, required: true,},
-    name:       {type: String},
-    business:   {type: String},
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  name: {type: String, required: true },
+  shopName: {type: String},
+  products: {
+    type: Schema.Types.ObjectId,
+    ref: "Products"
+  },
 });
 
 const User = mongoose.model("User", userSchema);
