dd9c6425cbd2954b29520e25b6c53210e20f81e7
Hannah Schuelke <hannah.r.schuelke@gmail.com>
2019-04-01 14:12:42 -0500

    working on landing page modal with Emily

---
+++ client/src/components/viewProduct/viewProductModal.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Modal from "react-bootstrap/Modal";
+
+
+const viewProductModal = props => (
+  <div className="viewProductModal">
+  {/* insert modal here */}
+  </div>
+);
+export default viewProductModal;
