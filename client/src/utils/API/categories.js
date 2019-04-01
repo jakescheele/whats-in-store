f33eb223439fb314e5ebeed783805e4ca7ec597c
Jake S <45572826+jakescheele@users.noreply.github.com>
2019-04-01 13:28:52 -0500

    Front end routes added

---
+++ client/src/utils/API/categories.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export default {
+    // CATEGORIES
+    // Gets all categories
+    getCategories: function () {
+        return axios.get("/api/categories");
+    },
+    // Gets the category with the given id
+    getCategory: function (id) {
+        return axios.get("/api/categories/" + id);
+    },
+    // Deletes the category with the given id
+    deleteCategory: function (id) {
+        return axios.delete("/api/categories/" + id);
+    },
+    // Saves a category to the database
+    saveCategory: function (catData) {
+        return axios.post("/api/categories", catData);
+    }
+};
