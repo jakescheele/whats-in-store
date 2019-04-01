ce1e50d4e1f6238a6d995857766bd995066362c3
Yuwen Li <muzlee1113@gmail.com>
2019-04-01 15:34:12 -0500

    nav bar and jumbotron

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Jumbotron from "../components/Jumbotron";
+
+function Promos(){
+    return(<Jumbotron pageName="PROFILE" instructions="Edit basic information of the shop."/>
+    )
+}
+
+export default Promos;
