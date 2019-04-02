195409ce60fc368734d639b132166b0395eeafc9
ercasale97 <ercasale97@gmail.com>
2019-04-02 14:02:32 -0500

    local modal changes

--- models/product.js
+++ models/product.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ProductSchema = new Schema({
+
+const productSchema = new Schema({
   name: { type: String, required: true },
   category: {
     type: Schema.Types.ObjectId,
@@ -17,6 +18,6 @@ const ProductSchema = new Schema({
   stock: { type: Array },
 });
 
-const Product = mongoose.model("product", ProductSchema);
+const Product = mongoose.model("Product", productSchema);
 
 module.exports = Product;
