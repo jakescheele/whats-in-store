43388bfe180b49f6ce7e0cf565078538ffb70859
ercasale97 <ercasale97@gmail.com>
2019-04-02 09:28:15 -0500

    add modal

--- client/src/pages/Inventory.js
+++ client/src/pages/Inventory.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Jumbotron from "../components/Jumbotron";
+import ModalComponent from "../components/ProductCardDetailed";
 
 
 function Inventory(){
     return(
         <Jumbotron pageName="INVENTORY" instructions="Click to view and edit products."/>
+        <div className="App">
+            <ModalComponent/>        
+        </div>
     )
 }
-
 export default Inventory;
