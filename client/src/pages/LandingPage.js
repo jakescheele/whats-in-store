53f3137595cff722ecc21d9632d34492a6543548
Yuwen Li <muzlee1113@gmail.com>
2019-04-04 10:37:55 -0500

    landing page

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -6,56 +6,47 @@ import Nav from "../components/NavBarWithoutHamburger";
 
 
 class LandingPage extends Component {
+    
     state = {
-        showLogin: false,
-        showSignup: false
+        LoginModal: false,
+        SignupModal: false
     }
 
-    showModalSignup = (event) => {
+    showModal= (modalName) => {
         this.setState({
-            showSignup: true
-        })
-    }
-
-    closeModalSignup = (event) => {
-        this.setState({
-            showSignup: false
-        })
-    }
-    showModalLogin = (event) => {
-        this.setState({
-            showLogin: true
+            [modalName]: true
         })
     }
 
-    closeModalLogin = (event) => {
+    closeModal = (modalName) => {
         this.setState({
-            showLogin: false
+            [modalName]: false
         })
     }
+   
 
     render() {
         return (
             <>
-            <Nav>
-                <Button variant="outline-light" className="mx-2 my-3 float-right" onClick={this.showModalLogin}>Login</Button>
-                <Button variant="outline-light" className="mx-2 my-3 float-right" onClick={this.showModalSignup}>Signup</Button>
-            </Nav>
-            <div className="vertical-center">
-                <Container className="text-light" >
-                    <Row>
-                        <Col>
-                            <div style={{"fontSize":"55pt"}}>Manage Your Inventory Online</div>
-                            <br></br>
-                            <h2>Whether your business is big or small, or you'd just like to keep your stuff straight.</h2>
-                            <br></br>
-                            <Button size="lg" variant="outline-light" onClick={this.showModalSignup}>Get Started</Button>
-                            <LoginModal modalState={this.state.showLogin} show={this.showModalLogin} close={this.closeModalLogin} />
-                            <SignupModal modalState={this.state.showSignup} show={this.showModalSignup} close={this.closeModalSignup} />
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
+                <Nav>
+                    <Button variant="outline-light" className="mx-2 my-3 float-right" onClick={(e)=>this.showModal("LoginModal")}>Login</Button>
+                    <Button variant="outline-light" className="mx-2 my-3 float-right" onClick={(e)=>this.showModal("SignupModal")}>Signup</Button>
+                </Nav>
+                <div className="vertical-center">
+                    <Container className="text-light">
+                        <Row>
+                            <Col>
+                                <div style={{ "fontSize": "55pt" }}>Manage Your Inventory Online</div>
+                                <br></br>
+                                <h2>Whether your business is big or small, or you'd just like to keep your stuff straight.</h2>
+                                <br></br>
+                                <Button size="lg" variant="outline-light" onClick={(e)=>this.showModal("SignupModal")}>Get Started</Button>
+                                <LoginModal modalState={this.state.LoginModal} show={this.showModal} close={this.closeModal} />
+                                <SignupModal modalState={this.state.SignupModal} show={this.showModal} close={this.closeModal} />
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
             </>)
     }
 }
