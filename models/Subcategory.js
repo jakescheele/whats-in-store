637808bc17f04baca7704eb819a3d35614c96599
Jonathan Hernandez <jon.hernandez169@gmail.com>
2019-04-04 14:36:54 -0500

    merging

--- models/Subcategory.js
+++ models/Subcategory.js
@@ -3,7 +3,11 @@ const Schema = mongoose.Schema;
 
 const subcategorySchema = new Schema({
   name: { type: String },
-  Category:{ type: String}
+  Category:{ type: String},
+  products:{
+    type:[String],
+    ref:'products'
+  }
 });
 
 const Subcategory = mongoose.model("Subcategory", subcategorySchema);
