578aed2828b6a17c32ff2fbde3a20dc79beed36e
Yuwen Li <muzlee1113@gmail.com>
2019-04-05 16:21:18 -0500

    fix the new product btn

--- client/src/components/ProductCardDetailed/index.js
+++ client/src/components/ProductCardDetailed/index.js
@@ -11,105 +11,6 @@ import ProductAPI from "../../utils/API/products"
 
 const emptyVariant = {name:"", stock:0};
 class ModalComponent extends React.Component {
-  // constructor(props) { //fixed
-  //   super(props); //fixed
-  //   this.state ={
-      
-  //       productid: "",
-  //       name: "",
-  //       category: "",
-  //       // subcategory:"",
-  //       // image:"",
-  //       price: "",
-  //       description: "",
-  //       stock: [{...emptyVariant}],
-  //       validated: false,
-  //   }
-  // }
-
-  
-  
-  
-
-  // // get data of the product fromt the DB
-  // componentDidMount () {
-  //   console.log("==========Here is product that is clicked==============")
-  //   console.log(this.props.product)
-  //   // set the clicked product info to state
-  //   // this.setState({
-  //   //   productid:  this.props.product._id,
-  //   //   name: this.props.product.name,
-  //   //   price: this.props.product.price,
-  //   //   description: this.props.product.description,
-  //   //   category: {...this.props.product.category},
-  //   // })
-  //   // save the stock array to state.stock
-  //   if (this.props.product.stock !== false)
-  //     this.setState({stock: this.props.product.stock})
-  //   // if (this.props.product.stock.length === 0) {
-  //   //   this.setState({stock: {...emptyVariant}})
-  //   // }
-  // }
-
-
-  // inputChangeHandler = (e) => {
-  //   let { name, value } = e.target
-  //   console.log(name)
-  //   console.log(value)
-  //   this.setState({
-  //     [name]: value
-  //   })
-  // }
-
-  // dropDownSelectHandler = (e)=>{
-  //   let { name, value } = e.target
-  //   console.log(name)
-  //   console.log(value)
-  //   this.setState({
-  //     [name]: value
-  //   })
-  // }
-
-  // updateVariant = (varientIndex, field) => value => {
-  //   this.setState(
-  //     {
-  //       stock: this.state.stock.map((item, i) => i===varientIndex? {...item, [field]: value} : {...item})
-  //     }
-  //   )
-  // }
-  
-  // addVariant = () => {
-  //   this.setState({
-  //     stock: [...this.props.product.stock, {...emptyVariant}]
-  //   })
-  // }
-
-
-  // handleSubmit = (event) => {
-    
-  //   // validation
-  //   const form = event.currentTarget
-  //   if (form.checkValidity() === false) {
-  //     event.preventDefault();
-  //     event.stopPropagation();
-  //   }
-  //   this.setState({ validated: true });
-
-  //   // make axio request to create new product
-  //   if (this.state.name && this.state.price && this.state.category){
-  //     console.log("=========here is the state=========")
-  //     console.log(this.state)
-  //     console.log("===================================")
-  //     console.log("Hello! I'm here trying to thit the save Product!")
-  //     ProductAPI.saveProduct(this.state)
-  //       .then(res => console.log(res))
-  //       .catch(err => console.log(err));
-
-  //     // close the modal after save changes
-  //     this.props.close("productModal")
-  //     window.location.reload()
-  //   }
-  // }
 
 
   render() {
@@ -132,7 +33,7 @@ class ModalComponent extends React.Component {
                 inputChangeHandler={this.props.inputChangeHandler} dropDownSelectHandler={this.props.dropDownSelectHandler}
                 // product info
                 productid={this.props._id}
-                productName={this.props.name} 
+                productName={this.props.productName} 
                 price={this.props.price} 
                 description = {this.props.description} 
                 selectedCategory={{...this.props.category}}
@@ -153,7 +54,7 @@ class ModalComponent extends React.Component {
             <Button variant="secondary" onClick={(e) => this.props.close("productModal")}>
               Close
             </Button>
-            <Button type="submit" variant="primary" onClick={this.handleSubmit}>
+            <Button type="submit" variant="primary" onClick={this.props.handleSubmit}>
               Save Changes
             </Button>
           </Modal.Footer>
