816e3d813ba634fcb50be22bcb7552750f8cf34b
Yuwen Li <muzlee1113@gmail.com>
2019-04-08 15:57:58 -0500

    fix center profile

--- client/src/components/BrandLogo/index.js
+++ client/src/components/BrandLogo/index.js
@@ -3,13 +3,16 @@ import React from "react";
 
 
 function BrandLogo(props) {
-    console.log(props.shop.shopName)
+
+    const shop = props.shop || {}
     return (<>
         <span id="brand">
          <img style={{"width":"120px", "height":"100%", "margin":"0px 0.5rem 0px 0.5rem","padding":"0px","objectFit":"scale-down"}} src={process.env.PUBLIC_URL + '/logo.png'}/>
-            <span className="font">                
-                {props.shop.shopName?(<span style={{"color":"white", "fontSize":"2vw", "margin":"2rem 0rem 0px 0.5rem"}}> <i className="fas fa-times pr-2"></i> {props.shop.shopName}</span>):("")}
-            </span>
+            {shop.hasOwnProperty("logo")?
+            (<span className="font text-light"> × <img style={{"width":"75px", "height":"75px", "margin":"0px 0.5rem 0px 0.5rem","padding":"0px","objectFit":"scale-down"}} src={shop.logo.logo_url}/></span>) 
+            :(shop.shopName?
+            (<span className="font" style={{"color":"white", "fontSize":"2vw", "margin":"2rem 0rem 0px 0.5rem"}}> × {shop.shopName}</span>)
+            :(""))}
         </span> 
     </>)
 }
