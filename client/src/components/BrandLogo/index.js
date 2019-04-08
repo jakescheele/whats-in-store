29d2fa00cf9aac78d3f46c557186fd9857638af1
Yuwen Li <muzlee1113@gmail.com>
2019-04-08 10:31:45 -0500

    sth

--- client/src/components/BrandLogo/index.js
+++ client/src/components/BrandLogo/index.js
@@ -7,7 +7,7 @@ function BrandLogo(props) {
         <span id="brand">
          <img style={{"width":"120px", "height":"100%", "margin":"0px 0.5rem 0px 0.5rem","padding":"0px","object-fit":"scale-down"}} src={process.env.PUBLIC_URL + '/logo.png'}/>
             <span className="font">                
-                {props.shopName?(<span style={{"color":"white", "fontSize":"24pt", "margin":"2rem 0rem 0px 0.5rem"}}> <i className="fas fa-times pr-2"></i> {props.shopName}</span>):("")}
+                {props.shopName?(<span style={{"color":"white", "fontSize":"2vw", "margin":"2rem 0rem 0px 0.5rem"}}> <i className="fas fa-times pr-2"></i> {props.shopName}</span>):("")}
             </span>
         </span> 
     </>)
